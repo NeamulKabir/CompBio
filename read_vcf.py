f3fe5456f7308012ff10f5727afe63e8dbe23d00
Mustafa Ümit ÖNER <onermustafaumit@gmail.com>
2018-03-21 17:33:54 +0800

    Add files via upload

--- read_vcf.py
+++ read_vcf.py
@@ -1,9 +1,20 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.metrics import precision_recall_fscore_support
 
-tumor_name = 'real1'
+def precision_recall_fscore(truth_label, predicted_label):
+	TP = np.sum(np.where((truth_label+predicted_label==2),1,0))
+	FP = np.sum(np.where((truth_label-predicted_label==-1),1,0))
+	FN = np.sum(np.where((truth_label-predicted_label==1),1,0))
+
+	precision = TP/(TP+FP)
+	recall = TP/(TP+FN)
+	fscore = (2*precision*recall)/(precision+recall)
+
+	return precision, recall, fscore
+
+
+tumor_name = 'syn5'
 print('Data:{}'.format(tumor_name))
 
 truth_file = tumor_name + '/' + tumor_name + '_truth.bed'
@@ -101,17 +112,17 @@ varscan_set = set(varscan_list)
 
 set_list=[truth_set,freebayes_set,mutect_set,vardict_set,varscan_set]
 m = len(set_list)
-# intersection_count = np.zeros((m,m))
-# for i in range(m):
-# 	first_set = set_list[i]
-# 	intersection_count[i,i] = len(first_set)
-# 	for j in range(i+1,m):
-# 		second_set = set_list[j]
-# 		intersection_count[i,j] = len(first_set.intersection(second_set))
+intersection_count = np.zeros((m,m))
+for i in range(m):
+	first_set = set_list[i]
+	intersection_count[i,i] = len(first_set)
+	for j in range(i+1,m):
+		second_set = set_list[j]
+		intersection_count[i,j] = len(first_set.intersection(second_set))
+print('Number of Predicted Positives: truth_set,freebayes_set,mutect_set,vardict_set,varscan_set')
+print(intersection_count)
 
-# print(intersection_count)
-
-union_set = freebayes_set | mutect_set | vardict_set | varscan_set
+union_set = truth_set | freebayes_set | mutect_set | vardict_set | varscan_set
 sorted_union_list = sorted(list(union_set))
 num_preds = len(sorted_union_list)
 
@@ -123,34 +134,56 @@ for i in range(num_preds):
 		if key in set_list[j]:
 			labels[i,j] = 1
 
-# labels_file = tumor_name + '/' + tumor_name +'_labels.txt'
-# with open(labels_file,'a') as f_labels_file:
-# 	f_labels_file.write('chrom\tloc\ttruth\tfreebayes\tmutect\tvardict\tvarscan\n')
-# 	for i in range(num_preds):
-# 		key = sorted_union_list[i]
-# 		chrom = key.split('_')[0]
-# 		loc = key.split('_')[1]
-# 		f_labels_file.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(chrom,loc,labels[i,0],labels[i,1],labels[i,2],labels[i,3],labels[i,4]))
+labels_file = tumor_name + '/' + tumor_name +'_labels.txt'
+with open(labels_file,'w') as f_labels_file:
+	f_labels_file.write('chrom\tloc\ttruth\tfreebayes\tmutect\tvardict\tvarscan\n')
+	for i in range(num_preds):
+		key = sorted_union_list[i]
+		chrom = key.split('_')[0]
+		loc = key.split('_')[1]
+		f_labels_file.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(chrom,loc,labels[i,0],labels[i,1],labels[i,2],labels[i,3],labels[i,4]))
 
 
 ############## FREEBAYES ##############
-precision, recall, f1_score, _ = precision_recall_fscore_support(labels[:,0], labels[:,1], average='macro')
+precision, recall, f1_score = precision_recall_fscore(labels[:,0], labels[:,1])
 print('############## FREEBAYES ##############')
 print('precision:{:.3f}, recall:{:.3f}, f1_score:{:.3f}\n'.format(precision,recall,f1_score))
 
 ############## MUTECT ##############
-precision, recall, f1_score, _ = precision_recall_fscore_support(labels[:,0], labels[:,2], average='macro')
+precision, recall, f1_score = precision_recall_fscore(labels[:,0], labels[:,2])
 print('############## MUTECT ##############')
 print('precision:{:.3f}, recall:{:.3f}, f1_score:{:.3f}\n'.format(precision,recall,f1_score))
 
 ############## VARDICT ##############
-precision, recall, f1_score, _ = precision_recall_fscore_support(labels[:,0], labels[:,3], average='macro')
+precision, recall, f1_score = precision_recall_fscore(labels[:,0], labels[:,3])
 print('############## VARDICT ##############')
 print('precision:{:.3f}, recall:{:.3f}, f1_score:{:.3f}\n'.format(precision,recall,f1_score))
 
 ############## VARSCAN ##############
-precision, recall, f1_score, _ = precision_recall_fscore_support(labels[:,0], labels[:,4], average='macro')
+precision, recall, f1_score = precision_recall_fscore(labels[:,0], labels[:,4])
 print('############## VARSCAN ##############')
 print('precision:{:.3f}, recall:{:.3f}, f1_score:{:.3f}\n'.format(precision,recall,f1_score))
+
+
+label_sum = np.sum(labels[:,1:],axis=1)
+############## VOTING: at least 2 of 4 ##############
+temp_label = np.where(label_sum >= 2,1,0)
+precision, recall, f1_score = precision_recall_fscore(labels[:,0], temp_label)
+print('############## VOTING: at least 2 of 4 ##############')
+print('precision:{:.3f}, recall:{:.3f}, f1_score:{:.3f}\n'.format(precision,recall,f1_score))
+
+############## VOTING: at least 3 of 4 ##############
+temp_label = np.where(label_sum >= 3,1,0)
+precision, recall, f1_score = precision_recall_fscore(labels[:,0], temp_label)
+print('############## VOTING: at least 3 of 4 ##############')
+print('precision:{:.3f}, recall:{:.3f}, f1_score:{:.3f}\n'.format(precision,recall,f1_score))
+
+############## VOTING: all 4 ##############
+temp_label = np.where(label_sum >= 4,1,0)
+precision, recall, f1_score = precision_recall_fscore(labels[:,0], temp_label)
+print('############## VOTING: all 4 ##############')
+print('precision:{:.3f}, recall:{:.3f}, f1_score:{:.3f}\n'.format(precision,recall,f1_score))
+
+
 
 
